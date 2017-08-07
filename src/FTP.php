6c985ee8a5f027f668099120e94d2a0a4cc0f451
Chris <christopherftw09@users.noreply.github.com>
2017-08-07 23:27:11 +0100

    Updated FTP Class Constructor

--- src/FTP.php
+++ src/FTP.php
@@ -5,51 +5,48 @@ namespace LaravelFTP;
 use Exception;
 
 /**
- * FTP Class
- * Laravel FTP Class - Based on CodeIgniter's FTP Class.
- * 
+ * Laravel FTP Class
+ * Based on CodeIgniter's FTP Class.
+ *
  * @license	http://opensource.org/licenses/MIT	MIT License
  */
 class FTP {
 	protected $mode;
 	protected $connection;
-	
+
 	/**
 	 * Class Constructor
-	 * 
-	 * @param	array	$config
-	 * 
-	 * @throws	\Exception
-	 * 
+	 *
+	 * @throws	Exception
+	 *
 	 * @return	bool
 	 */
-	public function __construct($hostname, $username, $password, $port = 21, $passive = true, $mode = FTP_ASCII)
+	public function __construct($hostname, $username, $password, $port = 21, $passive = true)
 	{
+		// Connecting to the ftp server.
 		$this->connection = @ftp_connect($hostname, $port);
-		try {
-			ftp_login($this->connection, $username, $password);
-			
-			// set passive mode if needed
-			ftp_pasv($this->connection, $passive);
-		}
-		catch(\Exception $e) {
-			throw new Exception($e->getMessage());
-		}
+		if($this->connection === false) throw new Exception('Unable to establish an connection with the FTP server.');
+
+		// Logging into ftp server.
+		if(!@ftp_login($this->connection, $username, $password)) throw new Exception('Login failed.');
+
+		// set passive mode if needed
+		ftp_pasv($this->connection, $passive);
 		return true;
 	}
-	
+
 	/**
 	 * Connection Checker
-	 * 
+	 *
 	 * Validates the ftp connection.
-	 * 
+	 *
 	 * @return	bool
 	 */
 	protected function _is_conn()
 	{
 		return is_resource($this->connection);
 	}
-	
+
 	/**
 	 * Change directory
 	 *
@@ -59,10 +56,10 @@ class FTP {
 	public function change_dir($path)
 	{
 		if(!$this->_is_conn()) return false;
-		
+
 		return @ftp_chdir($this->connection, $path);
 	}
-	
+
 	// create directory
 	// upload
 	// download
@@ -71,23 +68,23 @@ class FTP {
 	// delete_file($filepath)
 	// delete_dir($filepath)
 	// chmod($path, $perm)
-	
+
 	/**
 	 * Delete Directory
-	 * 
+	 *
 	 * deletes a folder and then recursively deletes everything (including sub-folders)
 	 * contained within it.
-	 * 
+	 *
 	 * @param	string	$filepath
 	 * @return	bool
 	 */
 	public function delete_dir($filepath)
 	{
 		if(!$this->_is_conn()) return false;
-		
+
 		// Add a trailing slash to the file path if needed
 		$filepath = preg_replace('/(.+?)\/*$/', '\\1/', $filepath);
-		
+
 		$list = $this->list_files($filepath);
 		if($list->isNotEmpty())
 		{
@@ -102,10 +99,10 @@ class FTP {
 		}
 		return @ftp_rmdir($this->connection, $filepath);
 	}
-	
+
 	/**
 	 * Listing the files in a specified directory.
-	 * 
+	 *
 	 * @param	string	$path
 	 * @return	array
 	 */
@@ -113,14 +110,14 @@ class FTP {
 	{
 		return $this->_is_conn()?collect(ftp_nlist($this->connection, $path)):false;
 	}
-	
+
 	// public function mirror($locpath, $rempath)
 	// protected function _getext($filename)
 	// protected function _settype($ext)
-	
+
 	// public function close()
 	// protected function _error($line)
-	
+
 	// function read_file($filepath)
 	// function save_file($filepath, $content)
 }
